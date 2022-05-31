a9486db77fd3b6e6108d5d022db518401860746d
Omotayo Ganiyu Seedik <60110209+Tdaycode@users.noreply.github.com>
2022-05-31 20:00:58 +0100

    adding instructions

---
+++ main.py
@@ -0,0 +1,4 @@
+import random
+print("GAME \nTITLE: ROCK PAPER SCISSORS GAME(RPS) \nROCK-R \nPAPER-P \nSCISSORS-S \n------------------------------------------------------ \n INSTRUCTIONS: \n1. Enter your choice \n2. Then the computer will choose a random choice \n3. Then the winner will be displayed \n4. The game will continue until you enter 'q' to quit")
+print("-------------------------------------------------------")
+print('GAME RULES\n1. Rock beats Scissors \n2. Scissors beats Paper \n3. Paper beats Rock')
